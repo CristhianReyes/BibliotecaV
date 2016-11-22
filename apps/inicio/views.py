d3673655814c2c55fcb045792dbdb73fd5008206
Cristhian <cristhian-9292@hotmail.com>
2016-11-21 22:38:35 -0600

    Creacion de Vistas

--- apps/inicio/views.py
+++ apps/inicio/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.views.generic import TemplateView
+
+def index(request):
+    return render_to_response('inicio/index.html')
+
+class index2(TemplateView):
+    template_name = 'inicio/index2.html'
