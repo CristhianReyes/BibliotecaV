0039834d24f756689102f93179556229d95528ba
Cristhian <cristhian-9292@hotmail.com>
2016-11-25 07:18:57 -0600

    Creacion de form libro y CRUD

--- apps/libros/urls.py
+++ apps/libros/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from .views import BuscarView, BusquedaView
+from . import views
 
 urlpatterns = patterns('',
 
 
 url(r'^buscar/$', BuscarView.as_view(), name = 'buscar'),
 url(r'^busqueda/$', BusquedaView.as_view()),
-
+url(r'^libros/$', views.libro_lista, name = 'libro_lista'),
+url (r'^libro/(?P<pk>[0-9]+)/$', views.libro_detalle, name='libro_detalle'),
+url (r'^libro/new/$', views.libro_nuevo, name='libro_nuevo'),
+url(r'^libro/(?P<pk>[0-9]+)/edit/$', views.libro_editar, name='libro_editar'),
+url(r'^libro/(?P<pk>[0-9]+)/borrar/$', views.libro_eliminar, name='libro_eliminar'),
 )
--- apps/libros/views.py
+++ apps/libros/views.py
@@ -1,8 +1,49 @@
 from django.views.generic import TemplateView, ListView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect,render_to_response
 from .models import Libro
+from .forms import LibroForm
 from apps.autores.models import Autor
 
+def libro_lista(request):
+    libros = Libro.objects.all
+    return render(request, 'libros/libro_lista.html', {'libros': libros})
+
+def libro_detalle(request, pk):
+    libros = get_object_or_404(Libro, pk=pk)
+    return render(request, 'libros/libro_detalle.html', {'libros': libros})
+
+def libro_nuevo(request):
+    if request.method == "POST":
+        form = LibroForm(request.POST, request.FILES)
+        if form.is_valid():
+            libro = form.save(commit=False)
+            libro = Libro(
+            #autor=form.cleaned_data['autor'],
+            nombre=form.cleaned_data['nombre'],
+            resumen=form.cleaned_data['resumen'],
+            portadas=request.FILES)
+            form.save()
+        return redirect('libro_lista')
+    else:
+        form = LibroForm()
+    return render(request, 'libros/libro_nuevo.html', {'form': form})
+
+def libro_editar(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+    if request.method == "POST":
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            libro = form.save(commit=False)
+            libro.save()
+            return redirect('libro_detalle', pk=libro.pk)
+    else:
+        form = LibroForm(instance=libro)
+    return render(request, 'libros/libro_editar.html', {'form': form})
+
+def libro_eliminar(request, pk):
+    libro = get_object_or_404(Libro,pk=pk)
+    libro.delete()
+    return redirect('libro_lista')
 
 class BuscarView(TemplateView):
 
